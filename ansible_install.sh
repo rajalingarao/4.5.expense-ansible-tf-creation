521af273552391f260828cf566c0bc8425c2896c
rajalingarao4545 <rajalingarao4545@gmail.com>
2026-05-08 19:03:44 +0530

    expense ansible tf creation

--- ansible_install.sh
+++ ansible_install.sh
@@ -4,5 +4,6 @@ cd /tmp
 git clone https://github.com/rajalingarao/4.4.expense-ansible.git
 cd 4.4.expense-ansible
 ansible-playbook -i inventory.ini db.yaml -e 'mysql_root_password=ExpenseApp@1'
+ansible-playbook -i inventory.ini db.yaml -e 'mysql_root_password=ExpenseApp@1'
 ansible-playbook -i inventory.ini backend.yaml
 ansible-playbook -i inventory.ini frontend.yaml
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
-output "instance_info" {
-    value = aws_instance.expense
-}
+# output "instance_info" {
+#     value = aws_instance.expense
+# }
